c155dd6474b27073a8834094a6994a254e2b45ea
Alexandre Sieg <sieg.alexandre@gmail.com>
2023-09-18 12:31:26 +0200

    feat(app): add configuration module and plant module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlantModule } from './plant/plant.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PlantModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
